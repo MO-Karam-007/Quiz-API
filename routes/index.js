d32758ca6fad9e75a5a37371573cd70d7ab39598
Mr-MK-dev <mkcoder66@gmail.com>
2023-05-08 17:21:53 +0300

    _

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.json({
-        msg: 'welcome, Follow next links to register',
-        
-        
+        msg: 'welcome',
     });
 });
 
