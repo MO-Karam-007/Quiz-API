de51a4b475840b4f8849dcd1f883a85ac6ba4be8
Mr-MK-dev <mkcoder66@gmail.com>
2023-06-09 18:46:59 +0300

    _v1lastExam

--- routes/quizes/quiz.Controller.js
+++ routes/quizes/quiz.Controller.js
@@ -6,12 +6,25 @@ const { generateToken } = require('../../middlewares/jwt');
 exports.getLastExam = async (req, res) => {
     try {
         const time = new Date(Date.now() - 3600000 * 24);
-        const quiz = await Quiz.find({
+        const lastMidTerm = await Quiz.findOne({
             status: 'publish',
+            category: 'mid_term',
             createdAt: { $gte: time },
-        });
+        }).sort({ createdAt: -1 });
 
-        res.send(quiz);
+        const lastFinal = await Quiz.findOne({
+            status: 'publish',
+            category: 'final',
+            createdAt: { $gte: time },
+        }).sort({ createdAt: -1 });
+        const lastQuiz = await Quiz.findOne({
+            status: 'publish',
+            category: 'final',
+            createdAt: { $gte: time },
+        }).sort({ createdAt: -1 });
+
+        const arr = { lastMidTerm, lastFinal, lastQuiz };
+        res.json({ arr });
     } catch (error) {
         res.json({
             status: 'fail',
