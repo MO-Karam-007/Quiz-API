6069192e6ab82d8af4ce5ba61e0bc24bb7465533
Mr-MK-dev <mkcoder66@gmail.com>
2023-08-05 10:17:27 +0300

    profilePic

--- routes/auth/auth.route.js
+++ routes/auth/auth.route.js
@@ -6,7 +6,15 @@ const { verfyToken } = require('../../middlewares/jwt');
 const multer = require('multer');
 
 // authRouter.route('/verify_user').get(authController.verify);
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Save images in the "uploads" folder
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname); // Set a unique filename for the uploaded image
+    },
+});
+const upload = multer({ storage: storage });
 
 authRouter.route('/signup').post(authController.register);
 authRouter.route('/login').post(authController.login);
@@ -15,11 +23,14 @@ authRouter.route('/verify').post(verfyToken, authController.verifyEmail);
 authRouter.route('/get_students').get(authController.getAllStd);
 authRouter
     .route('/complete_sign_up')
-    .get(verfyToken, authController.getSignedUpUser)
-    .put(
-        upload.single('profileImage'),
-        verfyToken,
-        authController.completeSignUp
-    );
+    .get(verfyToken, authController.getSignedUpUser);
+
+authRouter.put(
+    '/complete_sign_up',
+    upload.single('profileImage'),
+
+    verfyToken,
+    authController.completeSignUp
+);
 
 module.exports = authRouter;
